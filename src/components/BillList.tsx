a08ea48637a197a83bded239066d99ab01463706
David Donges <david.donges@mni.thm.de>
2019-04-21 11:59:07 +0200

    Fix state / props access in class-based component

--- src/components/BillList.tsx
+++ src/components/BillList.tsx
@@ -1,25 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-const mapStateToProps = (state: any) => {
-    return { bills: state.bills };
-};
-
-const BillList = ({ bills }) => (
-    <ul>
-        {bills.map((bill) => (
-            <li className="list-group-item" key="bill.id">{bill.title}</li>
-        ))}
-    </ul>
-);
-
-const List = connect(mapStateToProps)(BillList);
-
-export default List;
-
-/*
-import * as React from 'react';
-import { connect } from 'react-redux';
 import IBill from '../types/IBill';
 
 const mapStateToProps = (state: any) => {
@@ -33,7 +13,7 @@ interface IBillListState {
 class BillList extends React.Component<any, IBillListState> {
 
     public render() {
-        const { bills } = this.state;
+        const { bills } = this.props;
         return (
             <ul>
                 {bills.map((bill: any) => <li key={bill.id}>{bill.title}</li>)}
@@ -44,4 +24,3 @@ class BillList extends React.Component<any, IBillListState> {
 }
 
 export default connect(mapStateToProps)(BillList);
-*/
